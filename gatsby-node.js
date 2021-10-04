8b0ca48b6f64a3f3e022e5f54310e412886bc541
Preston So <pso@post.harvard.edu>
2021-10-04 15:04:38 -0300

    Define GraphQL client in gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,9 +11,45 @@
  *
  * See: https://www.gatsbyjs.com/docs/creating-a-local-plugin/#developing-a-local-plugin-that-is-outside-your-project
  */
-exports.onPreInit = () => console.log("Loaded gatsby-starter-plugin")
+const { ApolloClient } = require("apollo-client")
+const { InMemoryCache } = require("apollo-cache-inmemory")
+const { split } = require("apollo-link")
+const { HttpLink } = require("apollo-link-http")
+const { WebSocketLink } = require("apollo-link-ws")
+const { getMainDefinition } = require("apollo-utilities")
+const fetch = require("node-fetch")
+const gql = require("graphql-tag")
+const WebSocket = require("ws")
 
 const POST_NODE_TYPE = `Post`
+
+const client = new ApolloClient({
+  link: split(
+    ({ query }) => {
+      const definition = getMainDefinition(query)
+      return (
+        definition.kind === "OperationDefinition" &&
+        definition.operation === "subscription"
+      )
+    },
+    new WebSocketLink({
+      uri: `ws://localhost:4000`,
+      // or `ws://gatsby-source-plugin-api.glitch.me/`
+      options: {
+        reconnect: true,
+      },
+      webSocketImpl: WebSocket,
+    }),
+    new HttpLink({
+      uri: "http://localhost:4000",
+      // or `https://gatsby-source-plugin-api.glitch.me/`
+      fetch,
+    })
+  ),
+  cache: new InMemoryCache(),
+})
+
+exports.onPreInit = () => console.log("Loaded gatsby-starter-plugin")
 
 exports.sourceNodes = async ({
   actions,
